80f3bde6f6cd8ab0dfd67a3e43b9fa1c294c3426
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-04-06 11:42:32 +0530

    file changes

---
+++ shell_script/array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+animals=("cat" "dog" "elephant")
+
+echo "print the first element: ${animals[0]}"
+echo "print the second element: ${animals[1]}"
+echo "print the third element: ${animals[2]}"
