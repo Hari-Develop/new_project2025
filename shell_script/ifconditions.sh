7f016463608799c3c80bcb259a777726bd02da45
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-04-06 15:05:02 +0530

    file changes

--- shell_script/ifconditions.sh
+++ shell_script/ifconditions.sh
@@ -5,7 +5,7 @@ usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 echo "Disk usage is: ${usage}%"
 
 if [ "$usage" -ge 80 ]; then
-    echo "Disk usage is: ${usage}%"
+    echo "Disk usage is critically high!"
 else 
-    echo "Disk usage is: ${usage}%"
+    echo "Disk usage under control"
 fi
