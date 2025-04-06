bdc967deaf812294dfb7e3940c617b524e94a587
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-04-06 15:01:29 +0530

    file changes

--- shell_script/ifconditions.sh
+++ shell_script/ifconditions.sh
@@ -4,7 +4,7 @@ usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
 echo "Disk usage is: ${usage}%"
 
-if ["usage" -ge 80]; then
+if [ "$usage" -ge 80 ]; then
     echo "Disk usage is: ${usage}%"
 else 
     echo "Disk usage is: ${usage}%"
