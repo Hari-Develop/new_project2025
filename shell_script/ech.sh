3485673d49f0833c04fd458a15c77970c2601ad8
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-04-06 10:03:36 +0530

    file changes

--- shell_script/ech.sh
+++ shell_script/ech.sh
@@ -3,6 +3,6 @@
 PERSION_1=Rajesh
 PERSION_2=Hari
 
-echo "$PERSION_1 Hello World"
-echo "$PERSION_2 where are you from"
-echo "$PERSION_1 what are you doing"
+echo " ${PERSION_1} \$Hello World"
+echo " ${PERSION_2} where are you from"
+echo " ${PERSION_1} what are you doing"
