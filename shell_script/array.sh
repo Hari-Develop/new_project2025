7ab3bc1be115900fba89ece209d5d2193fe05a8c
Harinadha Reddy <hreddy@OSBIndia.COM>
2025-04-06 11:44:45 +0530

    file changes

--- shell_script/array.sh
+++ shell_script/array.sh
@@ -5,3 +5,4 @@ animals=("cat" "dog" "elephant")
 echo "print the first element: ${animals[0]}"
 echo "print the second element: ${animals[1]}"
 echo "print the third element: ${animals[2]}"
+echo "print the all the elemennts: ${animals[@]}"
